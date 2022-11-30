e83a8bc9b42139796e456de18b7b52d14e883735
SemihKaraagacli <KaraagacliSemih@gmail.com>
2022-11-30 23:50:15 +0300

    Admin Contact Page

---
+++ app/routes/admin/contact/create/create.tsx
@@ -0,0 +1,128 @@
+import { ActionFunction, MetaFunction } from "@remix-run/node";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faRightFromBracket,
+  faAnglesRight,
+} from "@fortawesome/free-solid-svg-icons";
+import { Form, Nav } from "react-bootstrap";
+import admin from "~/styles/admin.css";
+import { redirect } from "@remix-run/node";
+import { contactCreatePost } from "~/models/post.server";
+import background from "public/images/background.jpg";
+export function links() {
+  return [{ rel: "stylesheet", href: admin }];
+}
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Home-Create",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+export const action: ActionFunction = async ({ request }) => {
+  const formData = await request.formData();
+  const name = formData.get("name");
+  const adressName = formData.get("adressName");
+  const adressWebAdress = formData.get("adressWebAdress");
+  const google = formData.get("google");
+
+  await contactCreatePost({ name, adressName, adressWebAdress, google });
+  return redirect("/admin/contact");
+};
+
+export default function Index() {
+  return (
+    <>
+      <img className="home-img" src={background} alt="" />
+      <div className="home">
+        <div className="home-navbar">
+          <Nav className=" navi" activeKey="/home">
+            <div className="navii">
+              <Nav.Item>
+                <Nav.Link className="navi-link" href="/admin/home">
+                  MAİN
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link className="navi-link" href="/admin/about">
+                  ABOUT
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link className="navi-link" href="/admin/education">
+                  EDUCATİON
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link className="navi-link" href="/admin/experiance">
+                  EXPERİANCE
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link className="navi-link" href="/admin/projects">
+                  PROJECTS
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link className="navi-link" href="/admin/contact">
+                  CONTACT
+                </Nav.Link>
+              </Nav.Item>
+            </div>
+            <Nav.Item>
+              <Nav.Link className="exit" href="/">
+                <FontAwesomeIcon icon={faRightFromBracket} />
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </div>
+        <div className="head-row">
+          <div className="head-name">Create</div>
+          <a
+            className="flex flex-row items-center gap-1 no-underline"
+            href="../"
+          >
+            <div className="head-icon-name">Geri</div>
+            <FontAwesomeIcon className="head-icon" icon={faAnglesRight} />
+          </a>
+        </div>
+
+        <Form
+          className="flex flex-col items-center px-32"
+          method="post"
+          //   action=""
+        >
+          <input
+            type="text"
+            name="name"
+            placeholder="Name"
+            className="form-control mb-4"
+          />
+          <input
+            type="text"
+            name="adressName"
+            placeholder="adressName"
+            className="form-control mb-4"
+          />
+          <input
+            type="text"
+            name="adressWebAdress"
+            placeholder="adressWebAdress"
+            className="form-control mb-4"
+          />
+          <input
+            type="text"
+            name="google"
+            placeholder="google"
+            className="form-control mb-4"
+          />
+          <button
+            itemType="submit"
+            className="block bg-indigo-800 px-4 py-1 mt-4 rounded-md mb-7"
+          >
+            Save
+          </button>
+        </Form>
+      </div>
+    </>
+  );
+}
---
+++ app/routes/admin/contact/index.tsx
@@ -0,0 +1,130 @@
+import { Nav, Table } from "react-bootstrap";
+import { json, LoaderFunction, MetaFunction } from "@remix-run/node";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import admin from "~/styles/admin.css";
+import { useLoaderData } from "@remix-run/react";
+import type { contact } from "@prisma/client";
+import { db } from "~/utils/db.server";
+import background from "public/images/background.jpg";
+export function links() {
+  return [{ rel: "stylesheet", href: admin }];
+}
+export const meta: MetaFunction = () => ({
+  charset: "utf-8",
+  title: "Home",
+  viewport: "width=device-width,initial-scale=1",
+});
+
+type loaderData = { contact: Array<contact> };
+
+export const loader: LoaderFunction = async () => {
+  const data: loaderData = {
+    contact: await db.contact.findMany(),
+  };
+  return json(data);
+};
+
+export default function Index() {
+  const data = useLoaderData<loaderData>();
+  return (
+    <>
+      <img className="home-img" src={background} alt="" />
+      <div className="home">
+        <div className="home-navbar">
+          <Nav className=" navi" activeKey="/home">
+            <div className="navii">
+              <Nav.Item>
+                <Nav.Link className="navi-link" href="/admin/main">
+                  MAİN
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link className="navi-link" href="/admin/about">
+                  ABOUT
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link className="navi-link" href="/admin/education">
+                  EDUCATİON
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link className="navi-link" href="/admin/experiance">
+                  EXPERİANCE
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link className="navi-link" href="/admin/projects">
+                  PROJECTS
+                </Nav.Link>
+              </Nav.Item>
+              <Nav.Item>
+                <Nav.Link className="navi-link" href="/admin/contact">
+                  CONTACT
+                </Nav.Link>
+              </Nav.Item>
+            </div>
+            <Nav.Item>
+              <Nav.Link className="exit" href="/">
+                <FontAwesomeIcon icon={faRightFromBracket} />
+              </Nav.Link>
+            </Nav.Item>
+          </Nav>
+        </div>
+        <div className="home-tables">
+          <Table striped bordered hover variant="dark">
+            <thead className="text-center">
+              <tr>
+                <th>#</th>
+                <th>Name</th>
+                <th>Adress Name</th>
+                <th>Adress Web Adress</th>
+                <th>google</th>
+                <th>-</th>
+                <th>-</th>
+              </tr>
+            </thead>
+            <tbody className="text-center">
+              {data.contact
+                .sort((a, b) => a.id - b.id)
+                .map((contact) => (
+                  <tr>
+                    <td key={contact.id}>{contact.id}</td>
+                    <td key={contact.id}>{contact.name}</td>
+                    <td key={contact.id}>{contact.adressName}</td>
+                    <td key={contact.id}>{contact.adressWebAdress}</td>
+                    <td key={contact.id}>{contact.google}</td>
+                    <td>
+                      <div className="flex flex-col items-center ">
+                        <a
+                          className="remove"
+                          href={`/admin/contact/delete/${contact.id}`}
+                        >
+                          Remove
+                        </a>
+                      </div>
+                    </td>
+                    <td>
+                      <div className="flex flex-col items-center">
+                        <a
+                          className="update"
+                          href={`/admin/contact/update/${contact.id}`}
+                        >
+                          Update
+                        </a>
+                      </div>
+                    </td>
+                  </tr>
+                ))
+                .reverse()}
+            </tbody>
+          </Table>
+        </div>
+        <a className="create" href="/admin/contact/create/create">
+          Create
+        </a>
+      </div>
+    </>
+  );
+}
